3e6a6e73b267ac04e24f77c8acbde033eded7776
matiuskm <matiuskm@gmail.com>
2023-04-22 19:47:37 +0700

    add navigations

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,11 +12,30 @@
 
                 <!-- Navigation Links -->
                 @if(in_array(auth()->user()->role, ['admin', 'moderator']))
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('register')" :active="request()->routeIs('register')">
-                        {{ __('Buat user baru') }}
-                    </x-nav-link>
-                </div>
+                    <div class="hidden sm:flex sm:items-center sm:ml-6">
+                        <x-dropdown align="right" width="48">
+                            <x-slot name="trigger">
+                                <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                                    <div>Users</div>
+        
+                                    <div class="ml-1">
+                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                </button>
+                            </x-slot>
+        
+                            <x-slot name="content">
+                                <x-dropdown-link :href="route('register')">
+                                    {{ __('Buat User baru') }}
+                                </x-dropdown-link>
+                                <x-dropdown-link :href="route('user.index')">
+                                    {{ __('Daftar User') }}
+                                </x-dropdown-link>
+                            </x-slot>
+                        </x-dropdown>
+                    </div>
                 @endif
             </div>
 
@@ -74,8 +93,17 @@
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('timeline')" :active="request()->routeIs('timeline')">
-                {{ __('Timeline') }}
+                {{ __('Halaman Utama') }}
             </x-responsive-nav-link>
+            <div class="mt-3 space-y-1">
+                <x-responsive-nav-link :href="route('register')">
+                    {{ __('Buat User Baru') }}
+                </x-responsive-nav-link>
+
+                <x-responsive-nav-link :href="route('user.index')">
+                    {{ __('Daftar User') }}
+                </x-responsive-nav-link>
+            </div>
         </div>
 
         <!-- Responsive Settings Options -->
